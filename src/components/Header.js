643ee4c241f297703c3fa92250fe350b3950cf4a
Behzod Musurmonqulov <behzodmusurmonqulov@gmail.com>
2022-08-28 09:53:44 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <div className="app-header">
+            <h1>Note List</h1>
+            <header>
+                <label  id="dark-change"></label>
+            </header>
+        </div>
+    )
+}
+
+export default Header
